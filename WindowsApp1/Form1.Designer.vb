a0130b50e148732418f309c8d5449ccee001301f
Dan Penny <dpenny@clinicallogistics.net>
2017-09-26 23:43:56 -0300

    Init commit of assignment 2

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.bn_next = New System.Windows.Forms.Button()
+        Me.fname = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.tb_index = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'bn_next
+        '
+        Me.bn_next.Location = New System.Drawing.Point(173, 208)
+        Me.bn_next.Name = "bn_next"
+        Me.bn_next.Size = New System.Drawing.Size(75, 23)
+        Me.bn_next.TabIndex = 0
+        Me.bn_next.Text = "Next"
+        Me.bn_next.UseVisualStyleBackColor = True
+        '
+        'fname
+        '
+        Me.fname.Location = New System.Drawing.Point(147, 78)
+        Me.fname.Name = "fname"
+        Me.fname.Size = New System.Drawing.Size(100, 20)
+        Me.fname.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(89, 208)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Prev"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'tb_index
+        '
+        Me.tb_index.Location = New System.Drawing.Point(190, 13)
+        Me.tb_index.Name = "tb_index"
+        Me.tb_index.Size = New System.Drawing.Size(58, 20)
+        Me.tb_index.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(138, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "index"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(86, 81)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "first name"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.tb_index)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.fname)
+        Me.Controls.Add(Me.bn_next)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents bn_next As Button
+    Friend WithEvents fname As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tb_index As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+
+    Dim index As Integer = 0
+
+    'hashtable sample code
+    ' https://support.microsoft.com/en-ca/help/307933/how-to-work-with-the-hashtable-collection-in-visual-basic--net-or-in-v
+    '
+    Dim allData As New Hashtable
+
+    Dim RowIdxFirstName As Integer = 0
+
+    Private Sub NextButton_Click(sender As Object, e As EventArgs) Handles bn_next.Click
+        'TODO check for max
+
+        index = index + 1
+
+        updateData(index)
+
+    End Sub
+
+    Private Sub updateData(index As Integer)
+        Dim personData As ArrayList = allData(index)
+
+        fname.Text = personData.Item(RowIdxFirstName)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Sample code taken from
+        'https://docs.microsoft.com/en-us/dotnet/visual-basic/developing-apps/programming/drives-directories-files/how-to-read-from-comma-delimited-text-files
+        '
+        Using MyReader As New Microsoft.VisualBasic.
+                              FileIO.TextFieldParser(
+                                "C:\Users\dpenny\Documents\Visual Studio 2017\Projects\WindowsApp1\WindowsApp1\names.csv")
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(",")
+            Dim currentRow As String()
+            Dim rowNumber As Integer = 0
+            While Not MyReader.EndOfData
+                Try
+                    currentRow = MyReader.ReadFields()
+                    Dim rowData As New ArrayList
+                    Dim currentField As String
+                    For Each currentField In currentRow
+                        rowData.Add(currentField)
+                    Next
+                    allData.Add(rowNumber, rowData)
+                    rowNumber = rowNumber + 1
+                Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                    MsgBox("Line " & ex.Message & "is not valid and will be skipped.")
+                End Try
+            End While
+        End Using
+        updateData(0)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'TODO check for 0
+
+        index = index - 1
+
+        updateData(index)
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
